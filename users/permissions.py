18915c92ca2f2cd5ac3d16f9d70971fec24f45ef
fateme <fatemeh.mohammadi@dolanto..com.au>
2026-04-26 00:16:38 +0330

    feat: add user model and RBAC permissions

---
+++ users/permissions.py
@@ -0,0 +1,21 @@
+from rest_framework.permissions import BasePermission
+
+
+class IsAdmin(BasePermission):
+    """Full access - admin only"""
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and request.user.is_admin()
+
+
+class IsEditor(BasePermission):
+    """Upload and update - no delete"""
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and (
+            request.user.is_editor() or request.user.is_admin()
+        )
+
+
+class IsViewer(BasePermission):
+    """Read only"""
+    def has_permission(self, request, view):
+        return request.user.is_authenticated
